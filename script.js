83a6368f20200713504d460c119feedc21af92ad
Ajiyba İbrahim Bekir Bektaş <83498865+ajiybaibb@users.noreply.github.com>
2022-03-07 23:18:45 +0300

    codes added

---
+++ script.js
@@ -0,0 +1,8 @@
+function toplama(a,b)
+    {
+        document.write(a+b);
+    }
+var sayı1=Number(prompt("1. sayıyı giriniz"));
+var sayı2=Number(prompt("2. sayıyı giriniz"));
+
+toplama(sayı1,sayı2);
